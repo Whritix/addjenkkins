2c9d1657a8f72da1a460cb0bec3280735d496427
Whritix <01fe24bca043@kletech.ac.in>
2025-10-28 11:36:14 +0530

    jenkins2

---
+++ studentdetails.py
@@ -0,0 +1,8 @@
+student = {"id": 42, "name": "Srujan Mattur", "age": 19 , "grade": "F"}
+
+print(
+    f"ID:    {student['id']}\n"
+    f"Name:  {student['name']}\n"
+    f"Age:   {student['age']}\n"
+    f"Grade: {student['grade']}"
+)
